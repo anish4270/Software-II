34c26a1f050f3c33e274881fcb229bd986b8fc89
Jtrevino0897 <33116858+Jtrevino0897@users.noreply.github.com>
2020-02-14 23:33:41 -0600

    Add files via upload

---
+++ Addreservation.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwares2groupproject;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+public class Reservation {
+    reservation_node head;
+    
+    Reservation(){  
+        head = null;
+    }
+    static class reservation_node{
+        java.text.SimpleDateFormat reservation_date;
+        String name; 
+        reservation_node next;
+        reservation_node(java.text.SimpleDateFormat d, String n){
+            name = n;
+            d = reservation_date;
+            next = null;
+        }
+    }
+    //adding a reservation to the linked list
+    void addReservation(java.text.SimpleDateFormat d, String n){
+        reservation_node newNode = new reservation_node(d, n);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            reservation_node next = head;
+            reservation_node before = head;
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH");
+            Date date1 = sdf.parse(next.reservation_date);
+            Date date2 = sdf.parse(before.reservation_date);
+            while (next.reservation_date.before(newNode.reservation_date) < 0 && next.next != null){
+                before = next;
+                next = next.next;
+            }
+            newNode.next = next;
+            before.next = newNode;
+            System.out.println("Reservation added");
+        }
+    }
+}
